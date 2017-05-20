4035e9a91c5db8385f56e210fe3692ae7270aab5
ping <ping@users.noreply.github.com>
2017-05-20 16:57:37 +0800

    Try to fix deploy key

--- .github/build_ghpages.sh
+++ .github/build_ghpages.sh
@@ -9,8 +9,8 @@ TARGET_BRANCH="gh-pages"
 
 BUILD_DIR="$HOME/.build"
 
-# Pull requests and commits to other branches shouldn't try to deploy, just build to verify
-if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
+# Pull requests and commits to other branches shouldn't try to deploy, and only deploy in the php5.6 job
+if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" -o "$TRAVIS_PHP_VERSION" != "5.6" ]; then
     echo "Skipping gh-pages build."
     exit 0
 fi
