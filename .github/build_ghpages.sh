0b173e5d0d5c81d83505819709c99d9a1b260001
ping <ping@users.noreply.github.com>
2017-05-20 17:43:32 +0800

    Debug

--- .github/build_ghpages.sh
+++ .github/build_ghpages.sh
@@ -8,7 +8,7 @@ CWD=$(pwd)
 SOURCE_BRANCH="master"
 TARGET_BRANCH="gh-pages"
 
-BUILD_DIR="$CWD/.build"
+BUILD_DIR="$HOME/.build"
 
 # Pull requests and commits to other branches shouldn't try to deploy, and only deploy in the php5.6 job
 if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" -o "$TRAVIS_PHP_VERSION" != "5.6" ]; then
