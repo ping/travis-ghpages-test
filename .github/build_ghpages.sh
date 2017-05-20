b1a29a6e71dc21d50623885cb6ff53fd552f82b0
ping <ping@users.noreply.github.com>
2017-05-20 16:25:44 +0800

    Fix build script

--- .github/build_ghpages.sh
+++ .github/build_ghpages.sh
@@ -7,7 +7,7 @@ set -e
 SOURCE_BRANCH="master"
 TARGET_BRANCH="gh-pages"
 
-BUILD_DIR="$HOME/build"
+BUILD_DIR="$HOME/.build"
 
 # Pull requests and commits to other branches shouldn't try to deploy, just build to verify
 if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
